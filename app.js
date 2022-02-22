8e4e89c81a11d9141b3d49b5492ffe0092f657c9
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-02-22 08:07:34 -0500

    update

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
